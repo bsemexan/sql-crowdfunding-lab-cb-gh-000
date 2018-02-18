47dc40bbaf1aae21bd5a1f5e1d9c698e1df0a44d
Berry  Semexan <bsemexan@gmail.com>
2018-02-18 02:34:25 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,8 +8,9 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount
-FROM projects INNER JOIN pledges
-ON projects.id = pledges.project_id 
+FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
 ORDER BY title ASC;"
 end
 
